93854618c688cec440b142999643e4f37de00347
OksanaS94 <volodymyr.yastrubchak@gmail.com>
2019-06-25 19:54:22 +0300

    initial commint
    init

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+
+class App extends Component {
+
+  render () {
+    // TODO: add app global messages here with other global things
+
+    return (
+        <Fragment></Fragment>
+    //   <Fragment>
+    //     { this.props.children }
+    //   </Fragment>
+    )
+  }
+}
+
+export default connect(
+  (state) => ({ }),
+  dispatch => ({ })
+)(App);
