d88f27af0e0e3fdd4e685d0683a4b5d5415be54d
Liubomyr <yurchyshyn96@gmail.com>
2019-07-02 18:02:08 +0300

    123
    123

--- src/components/CreateAdvert/GeneralInfo/index.js
+++ src/components/CreateAdvert/GeneralInfo/index.js
@@ -3,7 +3,10 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 // import * as actions from '../../store/actions/';
-import styles from './style.modules.scss'
+import styles from './style.modules.scss';
+import TextArea from '../../common/TextArea';
+import Input from '../../common/Input';
+import UploadField from '../../common/UploadField';
 
 export class GeneralInfo extends Component {
 
@@ -13,12 +16,31 @@ export class GeneralInfo extends Component {
 
         return (
             <div className="general_info" >
-                
-                <h1>
-                    Загальна інформація
-                    
-                </h1>
+                <div className="title" >
+                    <span>1</span>
+                    <h1>Загальна інформація</h1>
+                </div>
 
+                <div className="first_position">
+                    <p className="subtitle">Назва організації:<span>*</span></p>
+                    <TextArea className="text_area" placeholder="Введіть назву організації" />
+                </div>
+
+                <div className="second_position">
+                    <p className="subtitle">Код ЄДРПОУ (8 цифр):<span>*</span></p>
+                    <Input className="input" placeholder="Введіть восьмизначний код" />
+                </div>
+
+                <div className="third_position">
+                    <p className="subtitle">Ціна купівлі без ПДВ та роздрібних витрат:<span>*</span></p>
+                    <Input className="input" placeholder="Ведіть ціну в гривнях" />
+                </div>
+
+                <div className="forth_position">
+
+                <UploadField />
+
+                </div>
             </div>
         );
     }
