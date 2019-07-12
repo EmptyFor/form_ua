2a9c6ba9c58174c8d63e4702529689d7d93e56d5
Liubomyr <yurchyshyn96@gmail.com>
2019-07-12 16:21:52 +0300

    1
    1

--- src/components/MainPage/Form/index.js
+++ src/components/MainPage/Form/index.js
@@ -16,59 +16,58 @@ const { Option } = Select;
 
 export class Form extends Component {
 
-render() {
-    return (
+    render() {
+        return (
 
-        <div className='wrapp' >
-            <h1>Розпочни свій бізнес вже сьогодні</h1>
-            <span >Знайди ідеальне рішення за декілька секунд...</span>
+            <div className='wrapp' >
+                <h1>Розпочни свій бізнес вже сьогодні</h1>
+                <span >Знайди ідеальне рішення за декілька секунд...</span>
 
-            <Select
-                showSearch
-                style={{ width: 200 }}
-                placeholder="Select a person"
-                optionFilterProp="children"
-
-                filterOption={(input, option) =>
-                    option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                }
-            >
-                <Option value="jack">Jack</Option>
-                <Option value="lucy">Lucy</Option>
-                <Option value="tom">Tom</Option>
-            </Select>,
-
+                <Select
+                    showSearch
+                    style={{ width: 200 }}
+                    placeholder="Select a person"
+                    optionFilterProp="children"
+                    filterOption={(input, option) =>
+                        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                    }
+                >
+                    <Option value="jack">Jack</Option>
+                    <Option value="lucy">Lucy</Option>
+                    <Option value="tom">Tom</Option>
+                </Select>
+              
                 <div className='search_form grid'>
 
-                {/*                   
+                    {/*                   
                     <Select width='auto' placeholder='Організаційно правова форма' icon={house_select_icon} type='multiply' id='select_1' />
                     <Select width='auto' placeholder='Організаційно правова форма' icon={house_select_icon} type='common' id='select_2' />
                     <Select width='auto' placeholder='Організаційно правова форма' icon={house_select_icon} type='common' id='select_3' />
                     <Select width='auto' placeholder='Організаційно правова форма' icon={house_select_icon} type='common' id='select_4' /> */}
 
-                <p className="price">Ціна</p>
-                <Input className='price_from' placeholder='від (₴)' />
-                <Input className='price_to' placeholder='до (₴)' />
+                    <p className="price">Ціна</p>
+                    <Input className='price_from' placeholder='від (₴)' />
+                    <Input className='price_to' placeholder='до (₴)' />
 
-                <div className="is_PDV_payer">
-                    <p>Є платником ПДВ</p>
-                    <Input type="checkbox" />
+                    <div className="is_PDV_payer">
+                        <p>Є платником ПДВ</p>
+                        <Input type="checkbox" />
+                    </div>
+
+
+                    <Button className='extendet_search '
+                        text='Розширений пошук' />
+
+
+                    <Link to={links.search} className='links'>
+                        <Button className='find'
+                            text='Знайти' />
+                    </Link>
                 </div>
+            </div >
 
-
-                <Button className='extendet_search '
-                    text='Розширений пошук' />
-
-
-                <Link to={links.search} className='links'>
-                    <Button className='find'
-                        text='Знайти' />
-                </Link>
-            </div>
-        </div >
-
-    );
-}
+        );
+    }
 }
 
 export default connect(
